6662a86b01e8c4d83c734eb980dacc23874efd97
alainaisawesome <alainarui@gmail.com>
2014-04-27 14:47:23 -0700

    pythonroom update

--- new.py
+++ new.py
@@ -1,1 +1,15 @@
 # author: alainaisawesome
+total = 0 
+month = 1
+days = 1
+year = 1900
+
+while year < 2015:
+	total = total + 1
+	if month == 12 and date == 31:
+		year = year + 1
+		month = 1
+		day = 1
+	else:
+		month = month + 1
+		

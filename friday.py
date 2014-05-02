cccfc83d7721372e2d31d08040379e542d87f7f5
alainaisawesome <alainarui@gmail.com>
2014-05-01 18:41:02 -0700

    pythonroom update

--- friday.py
+++ friday.py
@@ -4,10 +4,10 @@ print "welcome to fan world, today you will pick a fan!"
 input("what fan do you want to buy?")
 print "ok, here we go, you have three choices!"
 fans = [ " circle fan "," square fan "," diamond fan " ]
-for fan in fans:
+for fan, in fans:
 	answer = input("Do you want to buy" + fan + "?")
 	if answer == "yes":
-		print " then order" + fan + "good choice!"
+		print " then order" + fan, + ",good choice!"
 	if answer == "no":
 		print "Then don't order it!"
 		

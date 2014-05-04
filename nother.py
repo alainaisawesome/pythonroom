b2f40a7164273ba839eac6c365f4dfa10d79f02b
alainaisawesome <alainarui@gmail.com>
2014-05-04 15:18:22 -0700

    pythonroom update

--- nother.py
+++ nother.py
@@ -6,6 +6,8 @@ if word[0] == "who":
 	if word[1] == "are":
 		if word[2] == "you?":
 			print "Cleverbot 1 amazing."
-if word[0] == "what":
+if word[0] == "where":
+	if word[4] == "from?"
+		print "Botville, Botson"
 	
 	

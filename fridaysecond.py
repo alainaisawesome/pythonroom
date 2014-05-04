bd47b1c2fef16b8d739f39499ee0d548ed6a255a
alainaisawesome <alainarui@gmail.com>
2014-05-04 13:42:29 -0700

    pythonroom update

--- fridaysecond.py
+++ fridaysecond.py
@@ -1,11 +1,19 @@
 # author: alainaisawesome
 
 years = range(1900, 2015)
+months = range(1,13)
 total = 0
 
 for year in years:
-	total = total + 1
-	print year
+	for month in months:
+		days = range(1,32)
+		for day in days:
+				total = total + 1
+				for day in days:
+					
+	
+print total
+
 
 		
 

85ed50984f304d35a90f368dd8bb59b0c2af63f6
alainaisawesome <alainarui@gmail.com>
2014-05-04 15:16:00 -0700

    pythonroom update

---
+++ nother.py
@@ -0,0 +1,11 @@
+question = input("Speak human!")
+question = question.lower()
+word = question.split()
+
+if word[0] == "who":
+	if word[1] == "are":
+		if word[2] == "you?":
+			print "Cleverbot 1 amazing."
+if word[0] == "what":
+	
+	

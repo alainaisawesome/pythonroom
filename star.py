d38c249c97082d90b0c44525feaa94e9f96a5575
alainaisawesome <alainarui@gmail.com>
2014-05-11 14:18:43 -0700

    pythonroom update

--- star.py
+++ star.py
@@ -1,2 +1,23 @@
+import turtle
 
+
+world  = turtle.Screen()
+world.bgcolor("red")
+
+t1 = turtle.Turtle()
+t1.color("white")
+t1.left(30)
+t1.hideturtle()
+t2 = turtle.Turtle()
+t2.color("white")
+t2.left(150)
+t2.hideturtle()
+
+for i in range(22):
+	t1.left(i)
+	t1.forward(8)
 	
+
+for j in range(22):
+	t2.right(j)
+	t2.forward(8)

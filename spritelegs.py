1124753ef85a3971d4f80e8eff54653db14eea24
alainaisawesome <alainarui@gmail.com>
2014-05-11 14:35:49 -0700

    pythonroom update

--- spritelegs.py
+++ spritelegs.py
@@ -1,6 +1,11 @@
 import turtle
 
 t = turtle.Turtle()
+t.shape("
+n = int(input("How many legs should spidey have?")
+print m 
 
-n = input("How many legs should spidey have?")
-print n
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)

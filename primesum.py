c5b9e8fc063cc3efd01f38217fc3b07ae4786163
alainaisawesome <alainarui@gmail.com>
2014-05-11 15:09:04 -0700

    pythonroom update

--- primesum.py
+++ primesum.py
@@ -2,14 +2,14 @@ n = 500
 
 flowers = range(1, 1001)
 for flower in flowers:
-tests = range(2, n - 1)
-prime = True
-for t in tests:
-	if n % t == 0:
-		prime = False 
-
-if prime == True:
-	print str(n) + " is prime!"
+	tests = range(2, flower - 1)
+	prime = True
+	for t in tests:
+		if n % t == 0:
+			prime = False 
 	
-if prime == False:
-	print str(n) + " is not prime!"
+	if prime == True:
+		print str(n) + " is prime!"
+		
+	if prime == False:
+		print str(n) + " is not prime!"

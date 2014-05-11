3e1224ef6219c3460eea6a4b774b46c03e0dd47f
alainaisawesome <alainarui@gmail.com>
2014-05-11 13:11:24 -0700

    pythonroom update

--- star.py
+++ star.py
@@ -1,30 +1,3 @@
-question = input("Speak human!")
-question = question.lower()
-word = question.split()
 
-if word[0] == "who":
-	if word[1] == "are":
-		if word[2] == "you?":
-			print "Cleverbot 1 amazing."
-if word[0] == "where":
-	if word[1] == "are":
-		if word[2] == "you":
-			if word[3] == "from?":
-				print "Botville, Botson"
-if word[0] == "tell":
-	if word[1] == "me":
-		if word[2] == "how":
-			if word[3] == "it's":
-				if word[4] == "like":
-					if word[5] == "to":
-						if word[6] == "be":
-							if word[7] == "a":
-								if word[8] == "robot":
-									print "it's awesome!"
-if word[0] == "can":
-	if word [1] == "you":
-		if word[2] == "do":
-			if word[3] ==
-			
 	
 	

51ae51f0116255c7e22935de09e8e949d000fa9c
alainaisawesome <alainarui@gmail.com>
2014-05-11 15:14:42 -0700

    pythonroom update

--- primesum.py
+++ primesum.py
@@ -1,7 +1,7 @@
-n = 500
+total = 0
 
-for flower in range(1, 1001):
-	tests = range(2, flower - 1)
+for n in range(1, 1001):
+	tests = range(2, n - 1)
 	prime = True
 	for t in tests:
 		if n % t == 0:

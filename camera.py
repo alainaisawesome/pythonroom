b8ea0d7f60affb9ef62e5083125fa3b329f2585f
alainaisawesome <alainarui@gmail.com>
2014-05-25 15:32:06 -0700

    pythonroom update

--- camera.py
+++ camera.py
@@ -1,18 +1,5 @@
-import turtle
-t = turtle.Turtle()
-distances = range(10,100)
-colors = [ "red", "green", "blue", "pink" ]
-for distance in distances:
-	t.left(10)
-	for color in colors:
-		t.speed(0)
-		t.color(color)
-		t.forward(distance)
-		t.forward(10)
-		t.left(90)
-	
+#camera concious problem.
+n = int(input("how many people are in the room?"))
+c = int(input("how many flies are in the room?"))
 
-
-
-	
-	
+pictures = 0	

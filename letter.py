3edcaab30d0003807f508f14fb73de79ba92dab3
alainaisawesome <alainarui@gmail.com>
2014-05-25 13:28:39 -0700

    pythonroom update

--- letter.py
+++ letter.py
@@ -1,15 +1,8 @@
-import turtle
-t = turtle.Turtle()
-t.color("white")
-t.right(180)
-t.forward(90)
-t.color("black")
-t.left(75)
-t.forward(100)
-t.right(180)
-t.forward(50)
-t.right(77)
-t.forward(45)
-t.right(50)
+fibs = 0
+for n in range(1, 1000000):
+	fibs = fibs + 1
+	print fibs
+
+
 
 		

c7f59ffc29f1b64be009c803cefd3e32bd9706b1
alainaisawesome <alainarui@gmail.com>
2014-06-16 13:28:47 -0700

    pythonroom update

--- squaredpiral.py
+++ squaredpiral.py
@@ -1,15 +1,8 @@
 # author: alainaisawesome
 
-print "welcome to fan world, today you will pick a fan!"
-input("what fan do you want to buy?")
-print "ok, here we go, you have three choices!"
-fans = [ " circle fan "," square fan "," diamond fan " ]
-for fan, in fans:
-	answer = input("Do you want to buy" + fan + "?")
-	if answer == "yes":
-		print " then order" + fan, + ",good choice!"
-	if answer == "no":
-		print "Then don't order it!"
+import turtle
+t = turtle.Turtle()
+t.forward(100)
 		
 
 

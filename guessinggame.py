d7a6bb9c335db1de86a3af04b5dd9b2bc880407f
alainaisawesome <alainarui@gmail.com>
2014-06-16 16:00:39 -0700

    pythonroom update

--- guessinggame.py
+++ guessinggame.py
@@ -1,6 +1,27 @@
-# author: alainaisawesome
-print "think of a number in your head"
-
 import random
-answer = random.randint(1,100)
-print "answer"
+
+# author: alainaisawesome
+min_num = int(input("What is the minimum?"))
+max_num = int(input("What is the maximum?"))
+print "think of a number between " + str(min_num) + " and " + str(max_num)
+chances = int(input("How many chances do I get?"))  
+
+
+for i in range(chances):
+	answer = (min_num + max_num)/2
+	
+	print "My guess is " + str(answer)
+	
+	x = input ("is my answer correct, too low, or too high?")
+	
+	if x == "high":
+		max_num = answer
+	elif x == "low":
+		
+		min_num = answer
+	elif x == "correct":
+		print "I win"
+		break
+	else:
+		print "I don't recognize that number"
+	
